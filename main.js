5f2e5dac57f3fa051f3eec633b75bd4d0a08a1dc
shivam2399 <dubey.shivam2399@gmail.com>
2022-11-17 22:14:35 +0530

    stop storing uneccesary things

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ function saveToLocalStorage(event) {
     event.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    localStorage.setItem('name', name);
-    localStorage.setItem('email', email);
+    // localStorage.setItem('name', name);
+    // localStorage.setItem('email', email);
     const user = {
         name,
         email,
