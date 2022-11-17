6bebebfbe8b538b96f3fde48604d2ccb6942363a
shivam2399 <dubey.shivam2399@gmail.com>
2022-11-17 21:59:22 +0530

    Scaling the app to store multiple users

--- main.js
+++ main.js
@@ -8,6 +8,6 @@ function saveToLocalStorage(event) {
         name,
         email,
     };
-    localStorage.setItem('userDetails', JSON.stringify(user));
+    localStorage.setItem(name, JSON.stringify(user));
     
 }
