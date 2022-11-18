ae21317c76e3125055c0d6bea260c0ad0eceb040
shivam2399 <dubey.shivam2399@gmail.com>
2022-11-18 13:08:29 +0530

    Adding delete functionality

--- main.js
+++ main.js
@@ -12,13 +12,6 @@ function saveToLocalStorage(event) {
     showNewUserOnScreen(user);
 }
 
-function showNewUserOnScreen(user) {
-    const parentNode = document.getElementById('listOfUsers');
-    const childHTML = `<li> ${user.name} ${user.email} </li>`;
-
-    parentNode.innerHTML += childHTML;
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     Object.keys(localStorage).forEach((key) => {
 
@@ -30,3 +23,26 @@ document.addEventListener('DOMContentLoaded', function() {
         
         });
 })
+
+function showNewUserOnScreen(user) {
+    const parentNode = document.getElementById('listOfUsers');
+    const childHTML = `<li id=$(user.name)> ${user.name} - ${user.email} 
+                            <button onclick=deleteUser('${user.name}')> Delete </button>
+                        </li>`;
+    
+    console.log(childHTML);
+    parentNode.innerHTML += childHTML;
+    console.log(parentNode.innerHTML);
+}
+
+function deleteUser(name) {
+    localStorage.removeItem(name);
+    removeUserFromScreen(name);
+}
+
+function removeUserFromScreen(name) {
+    const parentNode = document.getElementById('listOfUsers');
+    const childNodeToBeDeleted = document.getElementById(name);
+    parentNode.removeChild(childNodeToBeDeleted);
+}
+
